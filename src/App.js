6912949c25171b0e4c62e43003313c96445b979c
Michael <osoba.michael@outlook.com>
2020-02-05 13:38:08 +0100

    Forgot to stage changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Provider } from './context';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layouts/Navbar';
 import Index from './components/layouts/Index';
@@ -6,18 +7,20 @@ import './App.css';
 import './bootstrap.min.css';
 
 class App extends Component {
-  render() {
+  render(){
     return (
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} ></Route>
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} ></Route>
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
